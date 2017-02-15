c114d3aebaeb130d47b41c287a4e6c0e236ee89b
poojapauskar <git.poojapauskar@gmail.com>
2017-02-15 12:37:51 +0530

    user page api added

--- new_user.php
+++ new_user.php
@@ -56,22 +56,339 @@
     display: none;
 }
   </style>
+
+<script>
+
+function proceed(){
+
+var a=document.forms["Form"]["uid"].value;
+if(a==null || a==''){
+        var text = "";
+        var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+        for( var i=0; i < 7; i++ )
+            text += possible.charAt(Math.floor(Math.random() * possible.length));
+
+        var mystring= (document.getElementById('name').value).substring(0, 3);
+        var uid_gen=mystring+text;
+        document.getElementById('uid').value = uid_gen;
+
+        alert("UID generated: "+uid_gen);
+}
+}
+</script>
 <head>
 <body>
-<form class="form-horizontal">
+
+<?php
+
+if(isset($_POST["generate_btn"]) and ($_POST['uid'] != '' || $_POST['uid'] != null)) {
+
+        $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        $charactersLength = strlen($characters);
+        $randomString = '';
+        for ($i = 0; $i < 4; $i++) {
+            $randomString .= $characters[rand(0, $charactersLength - 1)];
+        }
+
+        $names=array();
+        $names[0]= $randomString.rand(0, 9999).".jpg";
+        $names[1]= $randomString.rand(0, 9999).".jpg";
+        $names[2]= $randomString.rand(0, 9999).".jpg";
+        $names[3]= $randomString.rand(0, 9999).".jpg";
+        $names[4]= $randomString.rand(0, 9999).".jpg";
+        $names[5]= $randomString.rand(0, 9999).".jpg";
+        $names[6]= $randomString.rand(0, 9999).".jpg";
+
+
+        /*Get Signed Urls*/
+        $url = 'https://kyc-application.herokuapp.com/get_signed_url/';
+        $data = array('image_list' => [$names[0],$names[1],$names[2],$names[3],$names[4],$names[5],$names[6]]);
+
+        $options = array(
+          'http' => array(
+            'header'  => "Content-type: application/json\r\n",
+            'method'  => 'PUT',
+            'content' => json_encode($data),
+          ),
+        );
+        $context  = stream_context_create($options);
+        $result = file_get_contents($url, false, $context);
+        /*echo $result;*/
+        $arr = json_decode($result,true);
+
+        /*echo $arr[0][0];
+        echo $arr[0]['id'];
+        echo $arr[1][1];
+        echo $arr[1]['id'];*/
+    
+
+    $check = getimagesize($_FILES["pan_card"]["tmp_name"]);
+    if($check !== false) {
+        $url_upload = $arr[0][0];
+        /*echo $url_upload;*/
+
+
+        $filename = $_FILES["pan_card"]["tmp_name"];
+        $file = fopen($filename, "rb");
+        $data = fread($file, filesize($filename));
+
+        /*echo $data;*/
+
+        $options_upload = array(
+          'http' => array(
+            'header'  => "Content-type: \r\n",
+            'method'  => 'PUT',
+            'content' => $data,
+          ),
+        );
+        $context_upload  = stream_context_create($options_upload);
+        $result_upload = file_get_contents($url_upload, false, $context_upload);
+        /*var_dump($result_upload);*/
+        $arr_upload = json_decode($result_upload,true);
+        /*var_dump($arr_upload);*/
+
+        $pan_card_id=$arr[0]['id'];
+
+    } else {
+        $pan_card_id="";
+    }
+
+
+    $check_voter_id = getimagesize($_FILES["voter_id"]["tmp_name"]);
+    if($check_voter_id !== false) {
+        $url_upload_voter_id = $arr[1][1];
+        /*echo $url_upload;*/
+
+
+        $filename_voter_id = $_FILES["voter_id"]["tmp_name"];
+        $file_voter_id = fopen($filename_voter_id, "rb");
+        $data_voter_id = fread($file_voter_id, filesize($filename_voter_id));
+
+        /*echo $data;*/
+
+        $options_upload_voter_id = array(
+          'http' => array(
+            'header'  => "Content-type: \r\n",
+            'method'  => 'PUT',
+            'content' => $data_voter_id,
+          ),
+        );
+        $context_upload_voter_id  = stream_context_create($options_upload_voter_id);
+        $result_upload_voter_id = file_get_contents($url_upload_voter_id, false, $context_upload_voter_id);
+        /*var_dump($result_upload_voter_id);*/
+        $arr_upload_voter_id = json_decode($result_upload_voter_id,true);
+        /*var_dump($arr_upload_voter_id);*/
+
+        $voter_card_id=$arr[1]['id'];
+
+    } else {
+        $voter_card_id="";
+    }
+
+    $check_pass_book = getimagesize($_FILES["bank_pass_book"]["tmp_name"]);
+    if($check_pass_book !== false) {
+        $url_upload_pass_book = $arr[2][2];
+        /*echo $url_upload;*/
+
+
+        $filename_pass_book = $_FILES["bank_pass_book"]["tmp_name"];
+        $file_pass_book = fopen($filename_pass_book, "rb");
+        $data_pass_book = fread($file_pass_book, filesize($filename_pass_book));
+
+        /*echo $data;*/
+
+        $options_upload_pass_book = array(
+          'http' => array(
+            'header'  => "Content-type: \r\n",
+            'method'  => 'PUT',
+            'content' => $data_pass_book,
+          ),
+        );
+        $context_upload_pass_book  = stream_context_create($options_upload_pass_book);
+        $result_upload_pass_book = file_get_contents($url_upload_pass_book, false, $context_upload_pass_book);
+        /*var_dump($result_upload_pass_book);*/
+        $arr_upload_pass_book = json_decode($result_upload_pass_book,true);
+        /*var_dump($arr_upload_pass_book);*/
+
+        $pass_book_id=$arr[2]['id'];
+
+    } else {
+        $pass_book_id="";
+    }
+
+    $check_telephone_bill = getimagesize($_FILES["telephone_bill"]["tmp_name"]);
+    if($check_telephone_bill !== false) {
+        $url_upload_telephone_bill = $arr[3][3];
+        /*echo $url_upload;*/
+
+
+        $filename_telephone_bill = $_FILES["telephone_bill"]["tmp_name"];
+        $file_telephone_bill = fopen($filename_telephone_bill, "rb");
+        $data_telephone_bill = fread($file_telephone_bill, filesize($filename_telephone_bill));
+
+        /*echo $data;*/
+
+        $options_upload_telephone_bill = array(
+          'http' => array(
+            'header'  => "Content-type: \r\n",
+            'method'  => 'PUT',
+            'content' => $data_telephone_bill,
+          ),
+        );
+        $context_upload_telephone_bill  = stream_context_create($options_upload_telephone_bill);
+        $result_upload_telephone_bill = file_get_contents($url_upload_telephone_bill, false, $context_upload_telephone_bill);
+        /*var_dump($result_upload_telephone_bill);*/
+        $arr_upload_telephone_bill = json_decode($result_upload_telephone_bill,true);
+        /*var_dump($arr_upload_telephone_bill);*/
+
+        $telephone_bill_id=$arr[3]['id'];
+
+    } else {
+        $telephone_bill_id="";
+    }
+
+    $check_aadhar_card = getimagesize($_FILES["aadhar_card"]["tmp_name"]);
+    if($check_aadhar_card !== false) {
+        $url_upload_aadhar_card = $arr[4][4];
+        /*echo $url_upload;*/
+
+
+        $filename_aadhar_card = $_FILES["aadhar_card"]["tmp_name"];
+        $file_aadhar_card = fopen($filename_aadhar_card, "rb");
+        $data_aadhar_card = fread($file_aadhar_card, filesize($filename_aadhar_card));
+
+        /*echo $data;*/
+
+        $options_upload_aadhar_card = array(
+          'http' => array(
+            'header'  => "Content-type: \r\n",
+            'method'  => 'PUT',
+            'content' => $data_aadhar_card,
+          ),
+        );
+        $context_upload_aadhar_card  = stream_context_create($options_upload_aadhar_card);
+        $result_upload_aadhar_card = file_get_contents($url_upload_aadhar_card, false, $context_upload_aadhar_card);
+        /*var_dump($result_upload_aadhar_card);*/
+        $arr_upload_aadhar_card = json_decode($result_upload_aadhar_card,true);
+        /*var_dump($arr_upload_aadhar_card);*/
+
+        $aadhar_card_id=$arr[4]['id'];
+
+    } else {
+        $aadhar_card_id="";
+    }
+
+    $check_passport = getimagesize($_FILES["passport"]["tmp_name"]);
+    if($check_passport !== false) {
+        $url_upload_passport = $arr[5][5];
+        /*echo $url_upload;*/
+
+
+        $filename_passport = $_FILES["passport"]["tmp_name"];
+        $file_passport = fopen($filename_passport, "rb");
+        $data_passport = fread($file_passport, filesize($filename_passport));
+
+        /*echo $data;*/
+
+        $options_upload_passport = array(
+          'http' => array(
+            'header'  => "Content-type: \r\n",
+            'method'  => 'PUT',
+            'content' => $data_passport,
+          ),
+        );
+        $context_upload_passport  = stream_context_create($options_upload_passport);
+        $result_upload_passport = file_get_contents($url_upload_passport, false, $context_upload_passport);
+        /*var_dump($result_upload_passport);*/
+        $arr_upload_passport = json_decode($result_upload_passport,true);
+        /*var_dump($arr_upload_passport);*/
+
+        $passport_id=$arr[5]['id'];
+
+    } else {
+        $passport_id="";
+    }
+
+    $check_image = getimagesize($_FILES["image"]["tmp_name"]);
+    if($check_image !== false) {
+        $url_upload_image = $arr[6][6];
+        /*echo $url_upload;*/
+
+
+        $filename_image = $_FILES["image"]["tmp_name"];
+        $file_image = fopen($filename_image, "rb");
+        $data_image = fread($file_image, filesize($filename_image));
+
+        /*echo $data;*/
+
+        $options_upload_image = array(
+          'http' => array(
+            'header'  => "Content-type: \r\n",
+            'method'  => 'PUT',
+            'content' => $data_image,
+          ),
+        );
+        $context_upload_image  = stream_context_create($options_upload_image);
+        $result_upload_image = file_get_contents($url_upload_image, false, $context_upload_image);
+        /*var_dump($result_upload_image);*/
+        $arr_upload_image = json_decode($result_upload_image,true);
+        /*var_dump($arr_upload_image);*/
+
+        $image_id=$arr[6]['id'];
+
+    } else {
+        $image_id="";
+    }
+
+  $url_org = 'https://kyc-application.herokuapp.com/add_new_individual/';
+  $options_org = array(
+    'http' => array(
+      'header'  => array(
+                          'UID: '.$_POST['uid'],
+                          'NAME: '.$_POST['name'],
+                          'DOB: '.$_POST['date'],
+                          'PROFFESION: '.$_POST['profession'],
+                          'PAN: '.$_POST['pan'],
+                          'PAN-CARD: '.$pan_card_id,
+                          'ADDRESS: '.$_POST['address'],
+                          'TELEPHONE-BILL: '.$telephone_bill_id,
+                          'BANK-PASS-BOOK: '.$pass_book_id,
+                          'VOTER-ID: '.$voter_card_id,
+                          'PASSPORT: '.$passport_id,
+                          'AADHAR-NO: '.$aadhar_no,
+                          'AADHAR-CARD: '.$aadhar_card_id,
+                          'IMAGE: '.$image_id,
+                          ),
+      'method'  => 'GET',
+    ),
+  );
+  $context_org = stream_context_create($options_org);
+  $output_org = file_get_contents($url_org, false,$context_org);
+  $arr_org = json_decode($output_org,true);
+
+  if($arr_org['status']==200){
+    echo "<script>alert('New Individual Created')</script>";
+    $_POST = array();
+  }
+}
+?>
+
+
+<form onsubmit="return proceed();" name="Form" id="Form" class="form-horizontal" method="post" action="new_user.php" enctype="multipart/form-data">
 <fieldset>
 
 <!-- Form Name -->
 <legend><center>New Individual Entry</center></legend>
 <!--avatar upload-->
-<img class="profile-pic" style="margin-left:77%;position:absolute;z-index:2;" src="/images/boy-512.png" />
+<img class="profile-pic" style="margin-left:77%;position:absolute;z-index:2;" src="images/boy-512.png" />
 <div class="upload-button" style="position:absolute;z-index:2;margin-left:79%;margin-top:13%;">Upload Image</div>
-<input class="file-upload1" style="position:absolute;z-index:-2;margin-left:46%;margin-top:16%;" type="file">
+<input name="image" id="image" class="file-upload1" style="position:absolute;z-index:-2;margin-left:46%;margin-top:16%;" type="file">
 <!-- Text input-->
 <div class="form-group">
   <label class="col-md-4 control-label" for="textinput">UID:</label>  
   <div class="col-md-4">
-  <input id="textinput" name="textinput" type="text" placeholder="" class="form-control input-md">
+  <input id="uid" name="uid" type="text" value="<?php echo $_POST['uid'] ?>" placeholder="" class="form-control input-md">
     
   </div>
 </div>
@@ -80,7 +397,7 @@
 <div class="form-group">
   <label class="col-md-4 control-label" for="textinput">Name:</label>  
   <div class="col-md-4">
-  <input id="textinput" name="textinput" type="text" placeholder="" class="form-control input-md">
+  <input id="name" name="name" value="<?php echo $_POST['name'] ?>" type="text" placeholder="" class="form-control input-md">
     
   </div>
 </div>
@@ -88,7 +405,7 @@
 <div class="form-group row">
   <label for="example-date-input" class="col-2 col-form-label" style="margin-left:29.5%;">Date:</label>
   <div class="col-10">
-    <input class="form-control" style="width:31%;margin-left:34.6%;margin-top:-2%;" type="date" value="2011-08-19" id="example-date-input">
+    <input class="form-control" id="date" name="date" value="<?php echo $_POST['date'] ?>" style="width:31%;margin-left:34.6%;margin-top:-2%;" type="date" value="" id="example-date-input">
   </div>
 </div>
 
@@ -96,11 +413,12 @@
 <div class="form-group">
   <label class="col-md-4 control-label" for="selectbasic">Profession:</label>
   <div class="col-md-4">
-    <select id="selectbasic" name="selectbasic" class="form-control">
-      <option value="1">Option one</option>
-      <option value="2">Option two</option>
-      <option value="3">Option three</option>
-      <option value="4">Option four</option>
+    <select id="profession" name="profession" class="form-control">
+      <option value="Option one"><?php echo $_POST['profession'] ?></option>
+      <option value="Option one">Option one</option>
+      <option value="Option two">Option two</option>
+      <option value="Option three">Option three</option>
+      <option value="Option four">Option four</option>
     </select>
   </div>
 </div>
@@ -109,7 +427,7 @@
 <div class="form-group">
   <label class="col-md-4 control-label" for="textinput"></label>  
   <div class="col-md-4">
-  <input id="textinput" name="textinput" type="text" placeholder="specify " class="form-control input-md" required="">
+  <input id="textinput" name="textinput" type="text" placeholder="specify " class="form-control input-md" >
     
   </div>
 </div>
@@ -118,7 +436,7 @@
 <div class="form-group">
   <label class="col-md-4 control-label" for="textarea">Address:</label>
   <div class="col-md-4">                     
-    <textarea class="form-control" id="textarea" name="textarea">default text</textarea>
+    <textarea class="form-control" id="address" name="address" value="<?php echo $_POST['address'] ?>">default text</textarea>
   </div>
 </div>
 
@@ -126,7 +444,7 @@
 <div class="form-group">
   <label class="col-md-4 control-label" for="textinput">PAN:</label>  
   <div class="col-md-4">
-  <input id="textinput" name="textinput" type="text" placeholder="" class="form-control input-md">
+  <input id="pan" name="pan" value="<?php echo $_POST['pan'] ?>" type="text" placeholder="" class="form-control input-md">
     
   </div>
 </div>
@@ -135,7 +453,7 @@
 <div class="form-group">
   <label class="col-md-4 control-label" for="filebutton">PAN card:</label>
   <div class="col-md-4">
-    <input id="filebutton" name="filebutton" class="input-file" type="file">
+    <input id="pan_card" name="pan_card" value="<?php echo $_POST['pan_card'] ?>" class="input-file" type="file">
   </div>
 </div>
 
@@ -145,7 +463,7 @@
   <div class="col-md-4">
    <label class="checkbox-inline" for="checkboxes-0">
      <input type="checkbox" name="checkboxes" id="checkboxes-0" value="1">Telephone</label>
-<input id="filebutton" style="margin-top: -20px;margin-left: 129px;" name="filebutton" class="input-file" type="file">     
+<input id="telephone_bill"  value="<?php echo $_POST['telephone_bill'] ?>" style="margin-top: -20px;margin-left: 129px;" name="telephone_bill" class="input-file" type="file">     
  </div>
 </div>
 
@@ -155,7 +473,7 @@
  <div class="col-md-4">
    <label class="checkbox-inline" for="checkboxes-0">
      <input type="checkbox" name="checkboxes" id="checkboxes-0" value="1">Bank Passbook</label>
-<input id="filebutton" style="margin-top: -22px;margin-left: 129px;" name="filebutton" class="input-file" type="file">     
+<input id="bank_pass_book"  value="<?php echo $_POST['bank_pass_book'] ?>" style="margin-top: -22px;margin-left: 129px;" name="bank_pass_book" class="input-file" type="file">     
  </div>
 </div>
 
@@ -167,7 +485,7 @@
   <div class="col-md-4">
    <label class="checkbox-inline" for="checkboxes-0">
      <input type="checkbox" name="checkboxes" id="checkboxes-0" value="1">voter ID</label>
-<input id="filebutton" style="margin-top: -20px;margin-left: 129px;" name="filebutton" class="input-file" type="file">     
+<input id="voter_id" value="<?php echo $_POST['voter_id'] ?>" style="margin-top: -20px;margin-left: 129px;" name="voter_id" class="input-file" type="file">     
  </div>
 </div>
 
@@ -177,7 +495,7 @@
  <div class="col-md-4">
    <label class="checkbox-inline" for="checkboxes-0">
      <input type="checkbox" name="checkboxes" id="checkboxes-0" value="1">Passport</label>
-<input id="filebutton" style="margin-top: -22px;margin-left: 129px;" name="filebutton" class="input-file" type="file">     
+<input id="passport" value="<?php echo $_POST['passport'] ?>" style="margin-top: -22px;margin-left: 129px;" name="passport" class="input-file" type="file">     
  </div>
 </div>
 
@@ -185,7 +503,7 @@
 <div class="form-group">
   <label class="col-md-4 control-label" for="textinput">Adhar card No.</label>  
   <div class="col-md-4">
-  <input id="textinput" name="textinput" type="text" placeholder="" class="form-control input-md" required="">
+  <input id="aadhar_no" name="aadhar_no" value="<?php echo $_POST['aadhar_no'] ?>" type="text" placeholder="" class="form-control input-md" >
     
   </div>
 </div>
@@ -194,7 +512,7 @@
 <div class="form-group">
   <label class="col-md-4 control-label" for="filebutton">Adhar card:</label>
   <div class="col-md-4">
-    <input id="filebutton" name="filebutton" class="input-file" type="file">
+    <input id="aadhar_card" name="aadhar_card" value="<?php echo $_POST['aadhar_card'] ?>" class="input-file" type="file">
   </div>
 </div>
 
@@ -202,12 +520,15 @@
 <div class="form-group">
   <label class="col-md-4 control-label" for="singlebutton"></label>
   <div class="col-md-4">
-    <button id="singlebutton" name="singlebutton" class="btn btn-primary">Generate</button>
+    <button id="generate_btn" name="generate_btn" type="submit">Generate</button>
     <button id="singlebutton" style="margin-left:13%;" name="singlebutton" class="btn btn-primary">Discard</button>
   </div>
 </div>
 
 </fieldset>
 </form>
+
+
+
 </body>
 </html>
