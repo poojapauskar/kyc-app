186dc0d54a6f5d985de319a714d8e3776ca57371
poojapauskar <git.poojapauskar@gmail.com>
2017-02-23 14:43:23 +0530

    pg_pconnect

--- getautocomplete.php
+++ getautocomplete.php
@@ -1,5 +1,5 @@
 <?php
-$db = pg_connect("host=ec2-107-20-191-76.compute-1.amazonaws.com port=5432 dbname=deu9vahl80fvjn user=vdvqpruzihrics password=17b3e7a56da97ca021e3da54bb1694bb799849a2b5911014ed6caa05e1e4e02d");
+$db = pg_pconnect("host=ec2-107-20-191-76.compute-1.amazonaws.com port=5432 dbname=deu9vahl80fvjn user=vdvqpruzihrics password=17b3e7a56da97ca021e3da54bb1694bb799849a2b5911014ed6caa05e1e4e02d");
  pg_select($db, 'post_log', $_POST);
  	
  $term=$_GET["term"];
