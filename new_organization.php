9e1aa400e6031c2ebda4b8a306f1e7edf213f404
poojapauskar <git.poojapauskar@gmail.com>
2017-02-28 17:42:06 +0530

    title added for input PAN 2

--- new_organization.php
+++ new_organization.php
@@ -444,7 +444,7 @@ if(isset($_POST["save_btn"])) {
    <label class="checkbox-inline" for="checkboxes-0">
 
     <input type="checkbox" name="checkboxes" id="checkboxes-0" value="1" onchange="document.getElementById('telephone_bill').disabled = !this.checked;" >Telephone</label>
-     <div id="telephone_upload" style="width:68%;margin-left:32%;margin-top:-5%""/>
+     <div id="telephone_upload" style="width:68%;margin-left:32%;margin-top:-5%"/>
     <div class="fileUpload btn btn-info" style="margin-left:107%;margin-top:-6%;">
     <label style="font-weight:500;margin-bottom: 2px;">ATTACH</label>
     <input id="telephone_bill" name="telephone_bill" type="file" class="upload" onchange="telefilename(this.value);" /> 
