3a099c3564dc0c7e8daf94c1c8187775bb8eb6d1
Aiffin <aiffinsk@gmail.com>
2017-03-01 11:41:43 +0530

    resolved conflict in edit organ

--- edit_organization.php
+++ edit_organization.php
@@ -469,8 +469,8 @@ if(isset($_POST["edit_btn"])) {
 </div>
 
 <!-- Text input-->
-<div class="form-group">
-  <label class="col-md-4 control-label" for="textinput">PAN </label>  
+<div class="form-group" style="margin-top:-3%">
+  <label class="col-md-4 control-label" for="textinput" style="margin-left:-67%">PAN </label>
   <div class="col-md-4">
   <input id="pan" name="pan" value="<?php echo $arr_search['response'][0]['organization_details']['pan'] ?>" type="text" placeholder="PAN Card Number" class="form-control input-md">
     
