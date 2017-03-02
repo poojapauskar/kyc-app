66f6f4492973f73d2f77f0bbc12caf3fcd015cbf
poojapauskar <git.poojapauskar@gmail.com>
2017-03-02 16:35:47 +0530

    deleted comented part

--- search.php
+++ search.php
@@ -89,7 +89,7 @@
 
 <?php
 
-/* $db = pg_connect("host=ec2-107-20-191-76.compute-1.amazonaws.com port=5432 dbname=deu9vahl80fvjn user=vdvqpruzihrics password=17b3e7a56da97ca021e3da54bb1694bb799849a2b5911014ed6caa05e1e4e02d");
+$db = pg_connect("host=ec2-107-20-191-76.compute-1.amazonaws.com port=5432 dbname=deu9vahl80fvjn user=vdvqpruzihrics password=17b3e7a56da97ca021e3da54bb1694bb799849a2b5911014ed6caa05e1e4e02d");
  pg_select($db, 'post_log', $_POST);
  
 
@@ -98,16 +98,6 @@
  SELECT id,name,is_user FROM users_users");
 
  $json=array();
- // $json= array_values($json);
-//  while ($student = pg_fetch_array($query)) {
-//     $json["name"] = $student["name"];
-//     $json["id"] = $student["id"];
-// }
-  // while($student=pg_fetch_array($query)){
-  //        $json[]=array(
-  //                   'value'=> $student["name"],
-  //                   'id'=>$student["id"]);
-  //   }
 while ($student = pg_fetch_array($query)) {
     $json[$student["is_user"]."-".$student["id"]] = $student["name"];
 }
