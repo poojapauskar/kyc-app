553195ccc8771005362f1bbde0808bdf58dbe528
Aiffin <aiffinsk@gmail.com>
2017-03-03 12:25:32 +0530

    resloveddd

--- search_organization.php
+++ search_organization.php
@@ -146,10 +146,15 @@ $arr_search = json_decode($output_search,true);
   </div>
 </div> 
 
+<<<<<<< HEAD
 
 <div class="col-md-30" style="margin-top:-29%;position:static">
 <form class="form-horizontal" method="post" action="edit_organization.php" >
 
+=======
+<div class="col-md-30" style="margin-top:-29%;position:static">
+<form class="form-horizontal" method="post" action="edit_organization.php" >
+>>>>>>> search drop-down done
 
 <fieldset>
  <input type="hidden" value="<?php echo $arr_search['response'][0]['organization_details']['pk'] ?>" name="org_id" id="org_id"></input>
