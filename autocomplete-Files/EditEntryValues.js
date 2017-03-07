09e4db42721437a88603dcb39e7ca29ef7b6de33
Aiffin <aiffinsk@gmail.com>
2017-03-07 17:29:58 +0530

    enhancement issue #2 and #5 is done

--- autocomplete-Files/EditEntryValues.js
+++ autocomplete-Files/EditEntryValues.js
@@ -1,1 +1,1 @@
-var partnames={"124":"Amit","125":"girish","123":"Pooja Pauskar","126":"girish vr","127":"aaaa","128":"Pooja","129":"abc","130":"Test User","122":"","131":"popup","132":"amruta"}
+var partnames={"124":"Amit","125":"girish","123":"Pooja Pauskar","126":"girish vr","128":"Pooja","129":"abc","130":"Test User","131":"popup","133":"Pooja 123","134":"user 123","135":"User 777","136":"Rohit","122":""}
