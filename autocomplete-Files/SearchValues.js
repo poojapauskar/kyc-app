706b578cbab4684ae2ebf5fef421dde59fd85158
Aiffin <aiffinsk@gmail.com>
2017-03-09 14:34:26 +0530

    6th issue

--- autocomplete-Files/SearchValues.js
+++ autocomplete-Files/SearchValues.js
@@ -1,4 +1,1 @@
-
 var peoplenames={"0-52":"VRL","1-165":"","0-48":"Bitjini","1-158":"hritik","1-146":"Veena","1-163":"George","0-51":"Nike","1-169":"raisa","0-50":"Ford","1-162":"Albert","0-53":"Mahesh Foundation","1-144":"John","0-47":"Onida","1-156":"smita","0-55":"aiffinara baig","1-167":"Mahesh","1-143":"Pooja SP","0-54":"Bosch","1-159":"salman","1-166":"Karina","1-151":"Rohit","1-153":"Shahrouq","1-161":"raj","1-155":"rina","1-142":"Admin","1-168":"Aditi","1-152":"Supreet","1-157":"harsh","1-154":"Geeta","1-145":"Spoorti","1-147":"Uzma","1-160":"prerana","1-164":"Shweta"}
-
-
