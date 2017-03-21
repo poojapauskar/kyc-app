f4bc233522d1bafb06ae9e02345c71da9e5e3c2b
Aiffin <aiffinsk@gmail.com>
2017-03-21 16:13:42 +0530

    navigation bar is included in all pages

--- missing_reports.php
+++ missing_reports.php
@@ -1,26 +1,25 @@
 <html>
   <head>
 
-
+<link rel="stylesheet" type="text/css" href="css/material.indigo-pink.min.css">
  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.1.0/material.min.css">
+ <link rel="stylesheet" href="css/bootstrap.css">
  <link rel="stylesheet" href="css/table.css"> 
  <script src="https://code.getmdl.io/1.3.0/material.min.js"></script>
     <link rel="stylesheet" href="css/material.css">
     <!-- <script src="javascript/material.min.js"></script> -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
     <!-- Material Design Lite -->
-    <script src="https://code.getmdl.io/1.3.0/material.min.js"></script>
-    <link rel="stylesheet" href="css/material.css">
+<script src="https://code.getmdl.io/1.3.0/material.min.js"></script>
+<link rel="stylesheet" href="css/material.css">
+<link rel="stylesheet" href="css/fileupload.css">
     <!-- Material Design icon font -->
-
-    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
-    
- <!-- <link rel="stylesheet" href="https://html.nkdev.info/_con/assets/_con/css/con-base.min.css">
- <link rel="stylesheet" href="https://html.nkdev.info/_con/assets/_con/css/con-base.min.css"> -->
+<link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
 <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
 <script src="https://cdn.datatables.net/1.10.13/js/dataTables.material.min.js"></script>
+  <script src="https://storage.googleapis.com/code.getmdl.io/1.0.6/material.min.js"></script>
 <script>
 $(document).ready(function() {
     $('#example').DataTable( {
@@ -177,16 +176,16 @@ $arr_missing_report = json_decode($output_missing_report,true);
 /*echo count($arr_missing_report);*/
 ?>
 
-<div class="demo-layout-transparent mdl-layout mdl-js-layout">
-      <header style="background-color:#08426a;height:110px;-webkit-box-shadow: 0 3px 6px rgba(0,0,0,0.16), 0 3px 6px rgba(0,0,0,0.23) !important;
+<div class="mdl-layout mdl-js-layout mdl-layout--fixed-header">
+  <header style="background-color:#08426a;height:110px;-webkit-box-shadow: 0 3px 6px rgba(0,0,0,0.16), 0 3px 6px rgba(0,0,0,0.23) !important;
      -moz-box-shadow: 0 3px 6px rgba(0,0,0,0.16), 0 3px 6px rgba(0,0,0,0.23) !important;
-     box-shadow: 0 3px 6px rgba(0,0,0,0.16), 0 3px 6px rgba(0,0,0,0.23) !important;" class="mdl-layout__header mdl-layout__header--transparent">
-        <div class="mdl-layout__header-row" >
-
+     box-shadow: 0 3px 6px rgba(0,0,0,0.16), 0 3px 6px rgba(0,0,0,0.23) !important;" class="mdl-layout__header">
+    <div class="mdl-layout__header-row" >
         <a href="search.php"><img style="margin-top:5%;margin-left:28px;width:50px;heigh:50px" src="images/green.png"></img></a>
-<h5 style="margin-left:35%;margin-top:9%;">Missing Report</h5>
+        <h5 style="margin-left:35%;margin-top:9%;">Missing Report</h5>
          <span class="mdl-layout-title" style="margin-left:26%;margin-top:7%;">KYCApp</span>
           <!-- Add spacer, to align navigation to the right -->
+          </div>
       </header>
       <div class="mdl-layout__drawer">
         <span class="mdl-layout-title">Title</span>
@@ -201,9 +200,7 @@ $arr_missing_report = json_decode($output_missing_report,true);
           <a class="mdl-navigation__link" href="">Contact</a>
         </nav>
       </div>
-        </div>
-      </header>
-     
+
  <div class="container">
   <div class="row" style="margin-top:4%;"> 
 
