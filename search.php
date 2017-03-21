ce29d19cdb561ae2ebf1365e93e04e877c4739a3
Aiffin <aiffinsk@gmail.com>
2017-03-21 16:21:38 +0530

    conflict resolved in search page

--- search.php
+++ search.php
@@ -2,11 +2,18 @@
   <head>
     <!---bootstrap-->
   <meta name="viewport" content="width=device-width, initial-scale=1">
+<<<<<<< HEAD
   <link rel="stylesheet" type="text/css" href="css/material.indigo-pink.min.css">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
   <link rel="stylesheet" type="text/css" href="autocomplete-Files/styles.css">
     <!-- Material Design Lite -->
   
+=======
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <link rel="stylesheet" type="text/css" href="autocomplete-Files/styles.css">
+    <!-- Material Design Lite -->
+  <link rel="stylesheet" type="text/css" href="css/material.indigo-pink.min.css">
+>>>>>>> search page navigation bar css included
   <script src="https://code.getmdl.io/1.3.0/material.min.js"></script>
   <link rel="stylesheet" href="css/material.css">
     <!-- Material Design icon font -->
