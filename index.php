99aeac9c03061c0c5a72a4d55fd9aa79c06fc56f
Aiffin <aiffinsk@gmail.com>
2017-03-22 15:57:41 +0530

    index page css support mozilla

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html>
 <head>
   <title>CA DATA BASE</title>
-    <link rel="stylesheet" type="text/css" href="css/material.indigo-pink.min.css">
+  <link rel="stylesheet" type="text/css" href="css/material.indigo-pink.min.css">
   <link rel="stylesheet" href="css/bootstrap.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
