9fb7a82b6752c5a502cbc2df4953232896ce2ad6
poojapauskar <git.poojapauskar@gmail.com>
2017-03-30 14:14:26 +0530

    search on account token added 2

--- search.php
+++ search.php
@@ -236,7 +236,7 @@ $db = pg_connect("host=ec2-54-243-252-91.compute-1.amazonaws.com port=5432 dbnam
 
  $query=pg_query("SELECT id,name,is_user,account_token FROM organization_organization 
   UNION 
- SELECT id,name,is_user,account_token FROM users_users WHERE account_token = '22532153'");
+ SELECT id,name,is_user,account_token FROM users_users WHERE account_token = '".$_SESSION['account_token']."'");
 
 /*echo $query;*/
 
