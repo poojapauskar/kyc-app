dc18abf3a15a1965caaa3b96f9e8d3fd20c269c6
mdshahrouq <mdshah1994@gmail.com>
2017-04-18 12:11:17 +0530

    resolvedsrk

--- new.php
+++ new.php
@@ -1338,9 +1338,7 @@ function enable_disable(that){
   <label class="col-md-4 control-label" for="textinput" >UID:</label>  
   <div class="col-md-4">
 
-
-  <input id="uid" name="uid" type="text" value="<?php echo $_POST['uid'] ?>" placeholder="" class="form-control input-md" style="width:70%" disabled>
-
+  <input id="uid" name="uid" type="text" value="<?php echo $_POST['uid'] ?>" placeholder="" class="form-control input-md" style="width:70%" readonly>
   
   <input type="hidden" name="uid_format" id="uid_format" value="<?php echo $arr_uid['uid'] ?>"></input>
   </div>
