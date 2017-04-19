c7c30c1d361549ec1f03a948fca36c1ebbcb23b9
Aiffin <aiffinsk@gmail.com>
2017-04-19 16:23:57 +0530

    conflict resolved in admin_page

--- admin_page.php
+++ admin_page.php
@@ -25,7 +25,51 @@ if(isset($_POST['submit'])){
     $context = stream_context_create($options);
     $output = file_get_contents($url, false,$context);
     /*echo $output_can_be_deleted_or_no;*/
-    echo $output;
+
+    /*echo $output;*/
+    $arr = json_decode($output,true);
+
+/*echo $arr;*/
+/*echo $arr['organization'];*/
+
+
+ 
+  
+  
+header("Content-type: application/octet-stream");  
+header("Content-Disposition: attachment; filename=Reoprt.xls");  
+header("Pragma: no-cache");  
+header("Expires: 0");  
+ 
+$columnHeader = '';  
+$setData = '';  
+$columnHeader = "Type of Organization" . "\t" . "Name" . "\t" . "Registration" . "\t" . "Address" . "\t";
+
+echo ucwords($columnHeader) . "\n"; 
+echo "<br>";  
+
+for($i=0;$i<count($arr['organization']);$i++){
+  $setData= $arr['organization'][$i]['type_of_org']. "\t" . $arr['organization'][$i]['name'] . "\t" . $arr['organization'][$i]['registration'] . "\t" . $arr['organization'][$i]['address'] . "\t"; 
+  echo $setData;
+  echo "<br>"; 
+}
+
+echo "<br>"; 
+echo "<br>"; 
+
+$columnHeader1 = '';  
+$setData1 = '';  
+$columnHeader1 = "UID" . "\t" . "Name" . "\t" . "Profession" . "\t" . "Address" . "\t";
+
+echo ucwords($columnHeader1) . "\n"; 
+echo "<br>";  
+
+for($i=0;$i<count($arr['users']);$i++){
+  $setData1= $arr['users'][$i]['uid']. "\t" . $arr['users'][$i]['name'] . "\t" . $arr['users'][$i]['proffesion'] . "\t" . $arr['users'][$i]['address'] . "\t"; 
+  echo $setData1;
+  echo "<br>"; 
+}
+
     /*$arr = json_decode($output,true);*/
     /*echo $arr_can_be_deleted_or_no['status'];*/
 }else{
