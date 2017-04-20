a253dbd740aba90de1e21dc398e16775bb811bec
Aiffin <aiffinsk@gmail.com>
2017-04-20 13:26:47 +0530

    conflict resolved

--- autocomplete-Files/NewEntryValues.js
+++ autocomplete-Files/NewEntryValues.js
@@ -1,1 +1,1 @@
-var partnames=[]
+var partnames={"369":"professio test","371":"profress1","373":"addmore2","372":"addmore1","374":"Pooja MH","375":"Aarti","376":"Pooja MK","377":"Karan KL","378":"Kavya","379":"Kavya RT","381":"Kriti UU","382":"veena b","143":"Pooja SP","380":"Pooja SP"}
