ef2576cd3b9ddb39d3d99a218cbc154ee10efd45
Aiffin <aiffinsk@gmail.com>
2017-04-25 11:33:14 +0530

    new organ page alignment

--- autocomplete-Files/EditEntryValues.js
+++ autocomplete-Files/EditEntryValues.js
@@ -1,1 +1,1 @@
-var partnames={"369":"professio test","371":"profress1","373":"addmore2","372":"addmore1","374":"Pooja MH","375":"Aarti","376":"Pooja MK","377":"Karan KL","378":"Kavya","379":"Kavya RT","381":"Kriti UU","382":"veena b","143":"Pooja SP","380":"Pooja SP"}
+var partnames={"369":"professio test","371":"profress1","373":"addmore2","372":"addmore1","374":"Pooja MH","375":"Aarti","376":"Pooja MK","377":"Karan KL","378":"Kavya","379":"Kavya RT","381":"Kriti UU","382":"veena b","143":"Pooja SP","380":"Pooja SP","383":"usma","384":"veena a","385":"atreya","387":"sharukh a"}
