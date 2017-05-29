22efce0ecab9aa9c9e65b62dc55e24ef1c7ac023
poojapauskar <git.poojapauskar@gmail.com>
2017-05-29 13:36:47 +0530

    sidebar links

--- all.php
+++ all.php
@@ -131,12 +131,13 @@ session_start();
       <div class="mdl-layout__drawer">
         <span class="mdl-layout-title">KYCAPP</span>
         <nav class="mdl-navigation">
-           <a class="mdl-navigation__link" href="search.php">Home</a>
-          <a class="mdl-navigation__link" href="new.php">New Entry</a>
-          <a class="mdl-navigation__link" href="missing_reports.php">Missing Documents</a>
-           <a class="mdl-navigation__link" href="search_on_status.php?status=Pending">Pending Fees</a>
-            <a class="mdl-navigation__link" href="search_on_status.php?status=Paid">Paid Fees</a>
-            <a class="mdl-navigation__link" href="student_info.php">List of all Students</a>
+          <a class="mdl-navigation__link" href="search.php">Home</a>
+          <a class="mdl-navigation__link" href="new.php?is_user=0">New Entry Organization</a>
+          <a class="mdl-navigation__link" href="new.php?is_user=1">New Entry Individual</a>
+          <a class="mdl-navigation__link" href="missing_reports.php">Missing Reports</a>
+          <a class="mdl-navigation__link" href="search_on_status.php?status=Work in process">Work In Process</a>
+          <a class="mdl-navigation__link" href="search_on_status.php?status=Pending">Pending</a>
+           <a class="mdl-navigation__link" href="search_on_status.php?status=Completed">Completed</a>
         <?php if($_SESSION['is_admin'] == 1){?>
           <a class="mdl-navigation__link" href="admin_page.php">Admin</a>
         <?php }?>
