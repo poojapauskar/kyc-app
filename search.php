fd8d1db7ecdaa77e927a913f021794ff927ea096
mdshahrouq <mdshah1994@gmail.com>
2017-06-12 11:16:42 +0530

    Added remaining screen resolu

--- search.php
+++ search.php
@@ -10,7 +10,7 @@ if($_SESSION['login_kyc_app'] == 1){
 
 <html>
   <head>
-    <!---boottstrap-->
+    <!---booottstrap-->
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
    <link rel="icon" type="image/png" sizes="36x36" href="images/green_icon.svg">
