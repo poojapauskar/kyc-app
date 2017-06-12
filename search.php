db4e3c6b30508628b20abcceb4fabdb26fb07657
poojapauskar <git.poojapauskar@gmail.com>
2017-06-12 16:08:38 +0530

    loading spinner added

--- search.php
+++ search.php
@@ -45,6 +45,29 @@ if($_SESSION['login_kyc_app'] == 1){
 
 <body style="background-color:#E8E8E8;">
 
+<div style="position:absolute;z-index:5000;margin-left:34.5%">
+<img id="loading_spinner" style="position:absolute;z-index:2;" src="images/loading-spinner.gif">
+</div>
+
+
+<script type="text/javascript">
+  $('#loading_spinner').show();
+
+
+$.ajax({
+    url: 'suggestion.php',
+    type: 'POST',
+    data: '',
+    dataType: 'html',
+    success: function(data) {
+        /*$('.my_update_panel').html(data);*/
+//Moved the hide event so it waits to run until the prior event completes
+//It hide the spinner immediately, without waiting, until I moved it here
+        $('#loading_spinner').hide();
+    }
+});
+</script>
+
 <?php
 
 if($_POST['is_user_delete'] != "" && $_POST['pk_delete'] != ""){
@@ -297,33 +320,7 @@ if($_POST['is_user_delete'] != "" && $_POST['pk_delete'] != ""){
 
      
 
-<?php
 
-session_start();
-
-$db = pg_connect("host=ec2-107-20-191-76.compute-1.amazonaws.com port=5432 dbname=deu9vahl80fvjn user=vdvqpruzihrics password=17b3e7a56da97ca021e3da54bb1694bb799849a2b5911014ed6caa05e1e4e02d");
- pg_select($db, 'post_log', $_POST);
- 
-
- $query=pg_query("(SELECT id,name,is_user,account_token,is_active,pan FROM organization_organization WHERE is_active = 'true' AND account_token = '".$_SESSION['account_token']."')
-  UNION 
- (SELECT id,name,is_user,account_token,is_active,pan FROM users_users WHERE is_active = 'true' AND account_token = '".$_SESSION['account_token']."')");
-
-/*echo $query;*/
-
- $json=array();
-while ($student = pg_fetch_array($query)) {
-    $json[$student["is_user"]."-".$student["id"]] = $student["name"]."-".$student['pan'];
-}
-
-$textval = json_encode($json);
-$foo = "var peoplenames=" . $textval;
-
-file_put_contents('autocomplete-Files/SearchValues.js', $foo);
-
- 
-
-?>
 
 <!-- Assignment Modal  -->
 
