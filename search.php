e8451732e509e1a8d3336749fdd5cb28b25f7ca8
poojapauskar <git.poojapauskar@gmail.com>
2017-07-05 13:09:43 +0530

    fopen in suggestion.php 4

--- search.php
+++ search.php
@@ -115,9 +115,9 @@ var Url="check_session_valid.php";
 
 <script type="text/javascript">
   /*$('#loading_spinner').show();*/
-  NProgress.start();
+ /* NProgress.start();
 
-window.onload = function () { NProgress.done(); }
+window.onload = function () { NProgress.done(); }*/
 
 /*$.ajax({
     url: 'suggestion.php',
@@ -153,6 +153,7 @@ session_start();
 $db = pg_connect("host=ec2-107-20-191-76.compute-1.amazonaws.com port=5432 dbname=deu9vahl80fvjn user=vdvqpruzihrics password=17b3e7a56da97ca021e3da54bb1694bb799849a2b5911014ed6caa05e1e4e02d");
  pg_select($db, 'post_log', $_POST);
  
+echo "<script>NProgress.start()</script>";
 
  $query=pg_query("(SELECT id,name,is_user,account_token,is_active,pan FROM organization_organization WHERE is_active = 'true' AND account_token = '".$_SESSION['account_token']."')
   UNION 
@@ -169,7 +170,7 @@ $textval = json_encode($json);
 $foo = "var peoplenames=" . $textval;
 
 file_put_contents('autocomplete-Files/'.$_SESSION['account_token'].'.js', $foo);
-/*echo "<script>location='search.php'</script>";*/
+echo "<script>NProgress.done()</script>";
  
 
 ?>
