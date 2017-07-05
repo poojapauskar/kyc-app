64112820028ba95d38061ba6ca86a30e90e75082
poojapauskar <git.poojapauskar@gmail.com>
2017-07-05 13:27:34 +0530

    suggestion.php added and open file

--- search.php
+++ search.php
@@ -129,7 +129,7 @@ window.onload = function () { NProgress.done(); }*/
     }
 });*/
 
-/*var Url1="search.php";
+var Url1="suggestion.php";
 
   $.ajax({
   type: "POST",
@@ -138,44 +138,14 @@ window.onload = function () { NProgress.done(); }*/
   data: {},
   success: function(){
     NProgress.start();
-  });
+  },
   complete: function(){
     NProgress.done();
-  });
-});*/
+  }
+});
 
 </script>
 
-<?php
-
-session_start();
-
-fopen('autocomplete-Files/'.$_SESSION['account_token'].'.js', 'w');
-
-$db = pg_connect("host=ec2-107-20-191-76.compute-1.amazonaws.com port=5432 dbname=deu9vahl80fvjn user=vdvqpruzihrics password=17b3e7a56da97ca021e3da54bb1694bb799849a2b5911014ed6caa05e1e4e02d");
- pg_select($db, 'post_log', $_POST);
- 
-echo "<script>NProgress.start()</script>";
-
- $query=pg_query("(SELECT id,name,is_user,account_token,is_active,pan FROM organization_organization WHERE is_active = 'true' AND account_token = '".$_SESSION['account_token']."')
-  UNION 
- (SELECT id,name,is_user,account_token,is_active,pan FROM users_users WHERE is_active = 'true' AND account_token = '".$_SESSION['account_token']."')");
-
-/*echo $query;*/
-
- $json=array();
-while ($student = pg_fetch_array($query)) {
-    $json[$student["is_user"]."-".$student["id"]] = $student["name"]."-".$student['pan'];
-}
-
-$textval = json_encode($json);
-$foo = "var peoplenames=" . $textval;
-
-file_put_contents('autocomplete-Files/'.$_SESSION['account_token'].'.js', $foo);
-echo "<script>NProgress.done()</script>";
- 
-
-?>
 <?php
 
 $file= "autocomplete-Files/".$_SESSION['account_token'].".js";
